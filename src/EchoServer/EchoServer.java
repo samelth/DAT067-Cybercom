a2a9751a3770645f90c9807faa385790a9d0833e
smeppen <35624213+smeppen@users.noreply.github.com>
2020-11-16 12:12:44 +0100

    Replaced tabs with spaces :)

--- src/EchoServer/EchoServer.java
+++ src/EchoServer/EchoServer.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class EchoServer extends Thread {
-	private DatagramSocket socket;
+    private DatagramSocket socket;
     private boolean running;
  
     public EchoServer(int port) throws SocketException {
@@ -19,41 +19,41 @@ public class EchoServer extends Thread {
  
     public void run() {
         while (this.running) {
-        	byte[] buf = new byte[256];
+            byte[] buf = new byte[256];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             try {
-				this.socket.receive(packet);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+                this.socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             InetAddress packetAddress = packet.getAddress();
             int packetPort = packet.getPort();
             String time = (new SimpleDateFormat("HH:mm:ss")).format(new Date());
             System.out.println(time + " - packet received from " + packetAddress + ":" + packetPort + "\n" + new String(packet.getData()));
             packet = new DatagramPacket(buf, buf.length, packetAddress, packetPort);
             try {
-				this.socket.send(packet);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+                this.socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             System.out.println("packet echoed");
         }
         this.socket.close();
     }
     
     public void close() {
-    	this.running = false;
+        this.running = false;
     }
     
     public static void main(String[] args) throws IOException {
-		EchoServer server = new EchoServer(2390); // listen to port 2390
-		server.start();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("server started. type \"close\" to close the server and exit the program.");
-		while(!"close".equals(scanner.next()));
-		scanner.close();
-		server.close();
-		System.out.println("server closed. exiting program");
-		System.exit(0);
-	}
+        EchoServer server = new EchoServer(2390); // listen to port 2390
+        server.start();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("server started. type \"close\" to close the server and exit the program.");
+        while(!"close".equals(scanner.next()));
+        scanner.close();
+        server.close();
+        System.out.println("server closed. exiting program");
+        System.exit(0);
+    }
 }
