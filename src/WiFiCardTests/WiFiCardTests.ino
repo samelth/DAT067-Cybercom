952df543838cf2d7e8484951cfa7a1fd7cb05b56
Karl Svensson <Svensson.Karl@iCloud.com>
2020-11-16 08:48:10 +0100

    Send packets to server from wifi card

--- src/WiFiCardTests/WiFiCardTests.ino
+++ src/WiFiCardTests/WiFiCardTests.ino
@@ -6,6 +6,14 @@ char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 int status = WL_IDLE_STATUS; //WiFi radio status
 
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+
+IPAddress timeServer(188, 149, 54, 45); // Victor's echo server
+const int PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+WiFiUDP Udp;
+
+
 void setup() {
   Serial.begin(9600);
   while(!Serial){
@@ -34,11 +42,63 @@ void setup() {
   Serial.print("Connected to network");
   printCurrentNet();
 
+  Serial.println("\nStarting connection to server...");
+  Udp.begin(localPort);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(3000);
+  if ( Udp.parsePacket()!=0 ) {
+      
+    // We've received a packet
+    Serial.println("packet received");
+    int chars = Udp.read(packetBuffer, PACKET_SIZE); // read the packet into the buffer
+    /*
+     * Convert the bytes to printable characters and print them.
+     * Terminate either on number of characters actually received 
+     * or a null character which means end of string.
+     * Both conditions are needed here to avoid out of bounds
+     * access of data.
+     */
+    for(int i=0;i<chars && packetBuffer[i] > 0;i++)
+    {
+      char b1[2];
+      sprintf(b1, "%c", packetBuffer[i]);
+      b1[1]=0;
+      Serial.print(b1);
+    }
+    Serial.println();
+  }
+  delay(7000); // wait ten seconds
+}
 
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  memset(packetBuffer, 0, PACKET_SIZE); // set all bytes in the buffer to 0
+  int counter = 0;
+  packetBuffer[counter++] = 'H'; 
+  packetBuffer[counter++] = 'e';
+  packetBuffer[counter++] = 'l';
+  packetBuffer[counter++] = 'l';
+  packetBuffer[counter++]  = 'o';
+  packetBuffer[counter++]  = ' ';
+  packetBuffer[counter++]  = 'w';
+  packetBuffer[counter++]  = 'o';
+  packetBuffer[counter++]  = 'r';
+  packetBuffer[counter++]  = 'l';
+  packetBuffer[counter++]  = 'd';
+  packetBuffer[counter++]  = 'W';
+  packetBuffer[counter++]  = 'i';
+  packetBuffer[counter++]  = 'F';
+  packetBuffer[counter++]  = 'i';
+
+  Udp.beginPacket(address, localPort);
+  Udp.write(packetBuffer, PACKET_SIZE);
+  Udp.endPacket();
+  Serial.println("packet sent");
 }
 
 void printWifiData(){
