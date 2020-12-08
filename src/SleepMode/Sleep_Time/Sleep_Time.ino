f1df3258103566a979461718e6fc6b5333342e26
Karl Svensson <Svensson.Karl@iCloud.com>
2020-12-08 14:59:37 +0100

    Updated sleep mode example
    Does not work yet, problem with connection.

--- src/SleepMode/Sleep_Time/Sleep_Time.ino
+++ src/SleepMode/Sleep_Time/Sleep_Time.ino
@@ -1,25 +1,43 @@
-
 #include <MKRNB.h>
+
 #include "arduino_secrets.h"
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+// PIN Number
 const char PINNUMBER[]     = SECRET_PINNUMBER;
+
 unsigned int localPort = 2390;      // Local port to listen for UDP packets
-IPAddress Server(85, 230, 107, 67);
+
+IPAddress VictorServer(188, 149, 54, 45); // Victor's echo server
+IPAddress SamuelServer(83, 252, 118, 131); // Samuel's echo server
+IPAddress KarlServer(85, 230, 107, 67); // Karl's echo server
+IPAddress WilliamsServer(188,148,194,26); // William's echo server
+
+String TAU = "00000110";
+String Active_time = "00001100";
+
 const int PACKET_SIZE = 128;
 
+byte packetSendBuffer[PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+byte packetReceiveBuffer[PACKET_SIZE];
+
+// Initialize the library instance
 GPRS gprs;
 NB nbAccess;
 NBScanner scanner;
-NBUDP Udp;
-String response;
 
-byte packetBuffer[PACKET_SIZE];
+//If current IP address is equal to this nullIP we need to reconnect before we send any data.
+IPAddress nullIP= IPAddress(0,0,0,0);
+int timer=10;
 
 //Packet identifiers
 char packetNr0='0';
 char packetNr1='0';
 char packetNr2='0';
 
-void setup() {
+// A UDP instance to let us send and receive packets over UDP
+NBUDP Udp;
+void setup()
+{
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -28,48 +46,110 @@ void setup() {
 
   Serial.println("Starting PacketEcho Client...");
   connect();
-  
-
- 
+  setupPsm(TAU, Active_time);
   Serial.println("\nStarting connection to server...");
   Udp.begin(localPort);
-
+  sendUDPpacket(KarlServer); //Send an UDP packet to an echo server.
 }
 
-void loop() {
+
+
+void loop()
+{
+  //When running the program, send 'p' via serial to access the pause menu.
+  if(Serial.read()==112){
+    Serial.println("Pause Menu, press a number! \n 1. Your IP address \n 2. Current signal strength \n 3. Current carrier \n 4. Reconnect");
+     delay(500);
+     while(Serial.read()!=112){
+      int s = Serial.read();
+      while(s!=-1){
+        switch(s){
+          case 49: 
+           Serial.println(gprs.getIPAddress());
+           break;
+         case 50:
+          Serial.println(scanner.getSignalStrength());
+          break;
+         case 51:
+          Serial.println(scanner.getCurrentCarrier());
+          break;
+         case 52:
+          connect();
+          break;
+         default:
+          break;
+        }
+        s=Serial.read();
+      }
+    }
+  }
+  /*
+  if(gprs.getIPAddress()== nullIP){
+    Serial.print("Connection lost.\n");
+    connect();
+    }
+  Serial.print("");
+  */
   
-  sendUDPpacket(Server);
-  delay(2000);
-  Serial.println("\nEntering SleepMode");
-  digitalWrite(LED_BUILTIN, LOW);
-  //LowPower.deepSleep(3000000)
- 
-  
+  delay(500); //Wait two seconds
+   if(Udp.parsePacket()!=0) {  //If there are incoming packets on buffer, print them FCFS.
+    Serial.print("Received ");
+    int chars = Udp.read(packetReceiveBuffer, PACKET_SIZE); // Read the packet into the buffer
+    /*
+     * Convert the bytes to printable characters and print them.
+     * Terminate either on number of characters actually received 
+     * or a null character which means end of string.
+     * Both conditions are needed here to avoid out of bounds
+     * access of data.
+     */
+    for(int i=0;i<chars && packetReceiveBuffer[i] > 0;i++)
+    {
+      char b1[2];
+      sprintf(b1, "%c", packetReceiveBuffer[i]);
+      b1[1]=0;
+      Serial.print(b1);
+    }
+    Serial.println();
+    timer=10;
+    if(Udp.parsePacket()==0){
+    sendUDPpacket(SamuelServer); //Send an UDP packet to an echo server.
+    }
+  }
+  if(timer>0){
+    timer--;
+    }else{
+      Serial.println("Sending timeoutpacket: \n");
+      sendUDPpacket(SamuelServer); //Send an UDP packet to an echo server.
+      timer=10;
+      }
+   
+  delay(500); // Wait one second
 }
 
-unsigned long sendUDPpacket(IPAddress& address){
-  memset(packetBuffer, 0, PACKET_SIZE);
+unsigned long sendUDPpacket(IPAddress& address)
+{
+  memset(packetSendBuffer, 0, PACKET_SIZE); // Set all bytes in the buffer to 0
   int counter = 0;
+  packetSendBuffer[counter++]  = 'P'; 
+  packetSendBuffer[counter++]  = 'a';
+  packetSendBuffer[counter++]  = 'c';
+  packetSendBuffer[counter++]  = 'k';
+  packetSendBuffer[counter++]  = 'e';
+  packetSendBuffer[counter++]  = 't';
+  packetSendBuffer[counter++]  = 'N';
+  packetSendBuffer[counter++]  = 'r';
+  packetSendBuffer[counter++]  = ':';
+  packetSendBuffer[counter++]  = ' ';
+  packetSendBuffer[counter++]  = packetNr2;
+  packetSendBuffer[counter++]  = packetNr1;
+  packetSendBuffer[counter++]  = packetNr0;
 
-  packetBuffer[counter++]  = 'P'; 
-  packetBuffer[counter++]  = 'a';
-  packetBuffer[counter++]  = 'c';
-  packetBuffer[counter++]  = 'k';
-  packetBuffer[counter++]  = 'e';
-  packetBuffer[counter++]  = 't';
-  packetBuffer[counter++]  = 'N';
-  packetBuffer[counter++]  = 'r';
-  packetBuffer[counter++]  = ':';
-  packetBuffer[counter++]  = ' ';
-  packetBuffer[counter++]  = packetNr2;
-  packetBuffer[counter++]  = packetNr1;
-  packetBuffer[counter++]  = packetNr0;
-
+  
   Udp.beginPacket(address, localPort);
-  Udp.write(packetBuffer, PACKET_SIZE);
+  Udp.write(packetSendBuffer, PACKET_SIZE);
   Udp.endPacket();
 
-   //Put the packet numbers to a printable string
+  //Put the packet numbers to a printable string
   char *pNr; 
   pNr =(char*) malloc(4*sizeof(char));
   pNr[0]=packetNr2;
@@ -108,19 +188,56 @@ void connect(){
         (gprs.attachGPRS() == GPRS_READY)) {
       connected = true;
     } else {
-      Serial.println("Not connected");
+      Serial.println("Not connected, trying again..");
       delay(1000);
     }
   }
   Serial.println("Success!");
+  }
+
+ //Request power saving mode
+ void setupPsm(String TAU, String AT){
+  String response;
   
-    Serial.println("Request PSM");
-  
-  MODEM.sendf("AT+CPSMS=1,,,\"00000110\",\"00011110\"");
-  MODEM.waitForResponse(2000);
-  MODEM.sendf("AT+UCPSMS?");
+  MODEM.send("AT+CFUN=0");
   MODEM.waitForResponse(2000, &response);
   Serial.println(response);
-  MODEM.sendf("AT+UPSMR=1");
-  delay(5000);  
-  }
+  
+  delay(2000);
+  
+  MODEM.send("AT+CPSMS=0");
+  MODEM.waitForResponse(2000, &response);
+  Serial.print(response);
+  
+  delay(2000);
+
+  MODEM.send("AT+UPSMR=1");
+  MODEM.waitForResponse(2000, &response);
+  Serial.print(response);
+  delay(2000);
+  
+  MODEM.sendf("AT+CPSMS=1,,,\"%s\",\"%s\"", TAU.c_str(), AT.c_str());
+  MODEM.waitForResponse(2000, &response);
+  Serial.print(response);
+  delay(2000);
+  
+  apply();
+  
+ }
+
+ bool apply()
+{
+  Serial.print("Applying changes and saving configuration: ");
+  MODEM.send("AT+CFUN=15");
+  MODEM.waitForResponse(5000);
+  delay(5000);
+
+  do {
+    delay(1000);
+    MODEM.noop();
+  } while (MODEM.waitForResponse(1000) != 1);
+
+  Serial.println("done.");
+  
+  return true;
+}
